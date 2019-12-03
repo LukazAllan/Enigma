ae7d29eed62d28ab92bd41df6b4a2d7d8b27d733
LukazAllan <38227482+LukazAllan@users.noreply.github.com>
2019-12-03 20:38:37 -0300

    Atualizacao

--- ondaaondn/rules.js
+++ ondaaondn/rules.js
@@ -1,27 +1,26 @@
 function indexpg(num){
-    //let a = prompt("Digite a Resposta:")
     var a = document.getElementById("resp").value.toLowerCase().replace(/\s/g,'')
     console.log(`${a}, ${window.location.href}`) 
-    if (a === "azul" && num === 1) {
-        window.alert("Certa Resposta")
-        window.location.href = "page2.html"
-    } else if (a === "voz" && num === 2) {
-        window.alert("Certa Resposta")
-        console.log('else if')
-        window.location.href = "page3.html"
-    } else if (a === "ecoa" && num === 3) {
-        window.alert("Certa Resposta")
-        console.log('else if')
-        //window.location.href = "page2.html"
-    } else if (a === "versos" && num === 4) {
-        window.alert("Certa Resposta")
-        console.log('else if')
-        //window.location.href = "page2.html"
-    } else if (a === "perplexos" && num === 5) {
-        window.alert("Certa Resposta")
-        console.log('else if')
-        //window.location.href = "page2.html"
-    } else {
-        alert("Incorreta")
+    if (num === 1) {
+        if (a === "azul"){
+            window.alert("Certa Resposta")
+            window.location.href = "page2.html";
+        } else {
+            window.alert('Incorreta')
+        }
+    } else if (num === 2) {
+        if (a === "escravos"){
+            window.alert("Certa Resposta")
+            window.location.href = "page3.html";
+        } else {
+            window.alert('Incorreta')
+        }
+    } else if (num === 3) {
+        if (a === "independencia"){
+            window.alert("Certa Resposta")
+            //window.location.href = "page4.html"
+        } else {
+            window.alert('Incorreta')
+        }
     }
 }
